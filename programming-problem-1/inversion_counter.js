3ef3dbf0d1d67ff21d5eab2382dcbd5ebc49deda
Patrick McElwee <pmm21@duke.edu>
2014-10-26 12:42:31 -0400

    Works for arrays of length 2

--- programming-problem-1/inversion_counter.js
+++ programming-problem-1/inversion_counter.js
@@ -2,28 +2,21 @@ var InversionCounter = function() {};
 
 InversionCounter.prototype.count = function(array) {
   var self = this;
-  var n = array.length;
 
-  var firstHalfResults = self.countAndSort(array.slice(0,((n/2) - 1)));
-  var secondHalfResults = self.countAndSort(array.slice( (n/2), n-1 ));
-
-  var mergeResults = self.mergeAndCount(firstHalfResults[1],
-                                        secondHalfResults[1]);
-
-  var accumulator = (firstHalfResults[0] + secondHalfResults[0] + mergeResults[0]);
-
-  return accumulator
+  results = self.countAndSort(array);
+  return results[0];
 };
 
 InversionCounter.prototype.countAndSort = function(array) {
+  var self = this;
   var n = array.length;
 
   if (n <= 1) {
-    return [0, [n]];
+    return [0, array];
   }
 
-  var firstHalfResults = self.countAndSort(array.slice(0,((n/2) - 1)));
-  var secondHalfResults = self.countAndSort(array.slice( (n/2), n-1 ));
+  var firstHalfResults = self.countAndSort(array.slice(0,((n/2))));
+  var secondHalfResults = self.countAndSort(array.slice( (n/2) , n));
   var mergeResults = self.mergeAndCount(firstHalfResults[1],
                                         secondHalfResults[1]);
 
@@ -33,7 +26,20 @@ InversionCounter.prototype.countAndSort = function(array) {
 };
 
 InversionCounter.prototype.mergeAndCount = function(sortedArray1, sortedArray2) {
-  return [0, [sortedArray1 + sortedArray2]];
+  console.log(sortedArray1);
+  console.log(sortedArray2);
+  var merged = [];
+  if (sortedArray1[0] < sortedArray2[0]) {
+    merged << sortedArray1[0];
+
+    merged
+    return([0, merged + sortedArray2])
+  } else {
+    merged << sortedArray2[0];
+
+    merged << sortedArray1[0];
+    return([1, merged])
+  };
 };
 
 module.exports = InversionCounter;
